f0be03fb3fbc3f38fbd1d76307e6d049561e885e
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-02-11 20:10:40 +0000

    Update task-3.1.2.js
    document write heloo world lol and comments

---
+++ module-3/lesson-1/scripts/task-3.1.2.js
@@ -0,0 +1,10 @@
+/*this 
+is a
+multi-line
+comment*/
+
+// this is a single line comment//
+
+alert("Hello World "); //  this is a pop up box
+console.log ("Hello from the console"); // this shows on the console
+document.write("Heloo world"); //this writes a message on the page
