08ab08d3e1dbcc06c30094275654e7c0fb616017
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-02-11 21:41:25 +0000

    Update task-3.1.6.js
    Supposed to be doing bills but just typed it in

---
+++ module-3/lesson-1/scripts/task-3.1.6.js
@@ -0,0 +1,5 @@
+var preTipTotal=30;
+var tipPercentage=0.1;
+var totalBill=preTipTotal * (1+tipPercentage);
+
+document.write ("Thankyou for your custom.  Your total bill including tip of £3 is £"+totalBill.toFixed(2));
