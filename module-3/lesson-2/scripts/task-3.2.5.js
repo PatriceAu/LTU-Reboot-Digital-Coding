f5728f8888bf5c7f607e000174f2273d235758be
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-02-16 20:30:53 +0000

    Update task-3.2.5.js
    else if and else

---
+++ module-3/lesson-2/scripts/task-3.2.5.js
@@ -0,0 +1,14 @@
+var temperature = 41;
+
+if (temperature < 0) {
+    console.log("Stay inside");
+}
+else if (temperature < 30) {
+    console.log("Put a hat and coat on");
+}
+else if (temperature < 50) {
+    console.log("Put a coat on");
+}
+else {
+    console.log("vest and shorts OK");
+}
