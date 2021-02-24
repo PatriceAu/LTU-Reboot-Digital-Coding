2e0a78d5401f573000da29fec2d07014c7507a15
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-02-24 21:29:55 +0000

    function
    in an object, first use of const

---
+++ module-3/lesson-4/scripts/task-3.4.5.js
@@ -0,0 +1,12 @@
+ const favouriteRecipe = {
+    recipeTitle: " Jacket potato",
+    servings: 1,
+    ingredients: ["jacket potato"],
+    directions: ["scrape", "stab", "heat", "turn", "heat"],
+    letsCook: function recipeTitle (favouriteRecipe) {}
+}
+
+
+
+
+console.log("I'm hungry! Let's Cook" + favouriteRecipe.recipeTitle);
