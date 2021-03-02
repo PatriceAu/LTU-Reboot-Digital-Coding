587b45fce08111a9e4cb23cc7bbfab75902fd53e
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-03-02 21:34:11 +0000

    preventing default

---
+++ module-3/lesson-6/scripts/task-3.6.4.js
@@ -0,0 +1,9 @@
+let WolvesholdingCity=document.querySelector("a");
+
+WolvesholdingCity.addEventListener("click", function(event) {
+    event.preventDefault();
+    alert("jinxed");
+    console.log(event);
+
+});
+
