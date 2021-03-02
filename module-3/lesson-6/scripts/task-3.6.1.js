635e399511a2b5450d48e61381d1e6164444857c
PatriceAu <77541845+PatriceAu@users.noreply.github.com>
2021-03-02 19:50:20 +0000

    query selector
    Tried to add United logo!

---
+++ module-3/lesson-6/scripts/task-3.6.1.js
@@ -0,0 +1,7 @@
+let paragraphs=document.querySelectorAll("p");
+let paragraph3 = paragraphs[3];
+paragraph3.style.border="4px solid red";
+
+let logo = document.querySelector("img");
+
+logo.setAttribute("src", "https://www.manutd.com/en");
